b5f51825f70939e49d3fe41757382981eca2433e
Jos3ph2911 <167340017+Jos3ph2911@users.noreply.github.com>
2025-11-01 18:41:18 -0600

    Delete Proyecto1/Semana5 directory

--- Proyecto1/Semana5/_index.php
+++
@@ -1,9 +0,0 @@
-<?php include_once "funciones.php"; ?>
-<html>
-    <head>
-        <title> Muestra de includes </title>
-    </head>
-    <body>
-        <?php echo ' <p>'.mayor(2,5).' </p>';?>
-    </body>
-</html>
--- Proyecto1/Semana5/form.php
+++
@@ -1,5 +0,0 @@
-<form action="upload.php" method="post"
-enctype="multipart/form-data">
-    <input type="file" name= "nameFile" id="nameFile">
-    <input type="submit" value= "Subir">
-</form>
